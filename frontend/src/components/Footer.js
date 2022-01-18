9858281a6ab81ce92e89496b4f75792d4d04da00
lera <valeralera22@icloud.com>
2022-01-19 00:12:07 +0300

    lesson_2. React

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,19 @@
+import React from "react";
+import '../App.css';
+
+const Footer = () => {
+    return (
+        <footer>
+            <h3>
+                REST
+            </h3>
+            <div className={'container'}>
+                <div>
+                    ©
+                </div>
+            </div>
+        </footer>
+    );
+};
+
+export default Footer
---
+++ frontend/src/components/Menu.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../App.css';
+
+const Menu = () => {
+    return (
+            <h3>
+                TODO
+            </h3>
+    );
+};
+
+
+export default Menu
