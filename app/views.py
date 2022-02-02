ae2049080cf876ba733e92102329d82b54137916
lera <valeralera22@icloud.com>
2022-02-03 01:00:51 +0300

    lesson_4

--- app/views.py
+++ app/views.py
@@ -2,15 +2,37 @@ from rest_framework.renderers import JSONRenderer, BrowsableAPIRenderer
 from rest_framework.viewsets import ModelViewSet
 from app.models import Project, TODO
 from app.serializers import ProjectSerializer, TODOSerializer
+from rest_framework.pagination import LimitOffsetPagination
+from .filters import ProjectFilter, TODOFilter
+from rest_framework.response import Response
+
+
+class ProjectPagination(LimitOffsetPagination):
+   default_limit = 10
+
+
+class TODOPagination(LimitOffsetPagination):
+   default_limit = 20
 
 
 class ProjectViewSet(ModelViewSet):
     renderer_classes = [JSONRenderer, BrowsableAPIRenderer]
     queryset = Project.objects.all()
     serializer_class = ProjectSerializer
+    pagination_class = ProjectPagination
+    filterset_class = ProjectFilter
 
 
 class TODOViewSet(ModelViewSet):
     renderer_classes = [JSONRenderer, BrowsableAPIRenderer]
     queryset = TODO.objects.all()
     serializer_class = TODOSerializer
+    pagination_class = TODOPagination
+    filterset_class = TODOFilter
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = self.get_serializer(instance)
+        instance.is_active = False if instance.is_active else True
+        instance.save()
+        return Response(serializer.data)
