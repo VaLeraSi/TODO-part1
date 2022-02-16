066b2ca9cef778747a12b2cb9b89f4588bf5eb8c
lera <valeralera22@icloud.com>
2022-02-16 22:08:38 +0300

    Lesson_6

--- notes/settings.py
+++ notes/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'users',
     'corsheaders',
     'app',
+    'rest_framework.authtoken',
 ]
 
 MIDDLEWARE = [
@@ -135,5 +136,13 @@ REST_FRAMEWORK = {
     ],
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 100
+    'PAGE_SIZE': 100,
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
+    ],
 }
